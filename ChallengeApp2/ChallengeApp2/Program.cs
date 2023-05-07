4922111937fa99cb3eb21a6ae3f5a1b3640214a4
sebekoo <sebekoo@o2.pl>
2023-05-07 21:33:33 +0200

    Zadanie domowe dzień 4

--- ChallengeApp2/ChallengeApp2/Program.cs
+++ ChallengeApp2/ChallengeApp2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+char sex = 'm';
+int age = 22;
+
+if (sex == 'k')
+{
+    if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else if (sex == 'k' && age <= 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else
+    {
+        Console.WriteLine("Kobieta");
+    }
+}
+else if (age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Pełnoletni mężczyzna");
+}

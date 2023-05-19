caf8e2ff443a35b03cd804e1b05700661081b503
sebekoo <sebekoo@o2.pl>
2023-05-19 21:18:28 +0200

    Zadanie domowe dzien 10

--- ChallengeApp2/ChallengeApp2/Employee.cs
+++ ChallengeApp2/ChallengeApp2/Employee.cs
@@ -3,18 +3,50 @@
     public class Employee
     {
         private List<float> grades = new List<float>();
-        public Employee(string name, string surname) 
-        { 
-            this.Name = name; 
-            this.Surname = surname; 
+        public Employee(string name, string surname)
+        {
+            this.Name = name;
+            this.Surname = surname;
         }
         public string Name { get; private set; }
         public string Surname { get; private set; }
-        
+
         public void AddGrade(float grade)
         {
-            this.grades.Add(grade);
+            if (grade >= 0 && grade <= 100)
+            {
+                grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("Invalid grade value");
+            }
         }
+
+        public void AddGrade(string grade)
+        {
+            if (float.TryParse(grade, out float result))
+            {
+                this.AddGrade(result);
+            }
+            else
+            {
+                Console.WriteLine("String is not float");
+            }
+        }
+
+        public void AddGrade(double grade)
+        {
+            float gradeAsFloat = (float)grade;
+            this.AddGrade(gradeAsFloat);
+        }
+
+        public void AddGrade(int grade)
+        {
+            float gradeAsFloat = (float)grade;
+            this.AddGrade(gradeAsFloat);
+        }
+
         public Statistics GetStatistics()
         {
             var statistics = new Statistics();
@@ -22,7 +54,7 @@
             statistics.Max = float.MinValue;
             statistics.Min = float.MaxValue;
 
-            foreach(var grade in this.grades)
+            foreach (var grade in this.grades)
             {
                 statistics.Max = Math.Max(statistics.Max, grade);
                 statistics.Min = Math.Min(statistics.Min, grade);
--- ChallengeApp2/ChallengeApp2/Program.cs
+++ ChallengeApp2/ChallengeApp2/Program.cs
@@ -1,10 +1,13 @@
 ﻿using ChallengeApp2;
 
 var employee = new Employee("Adam", "Nowak");
-employee.AddGrade(2);
-employee.AddGrade(2);
-employee.AddGrade(6);
+employee.AddGrade("Adam");
+employee.AddGrade("5");
+employee.AddGrade(2f);
+employee.AddGrade(6d);
 var statistics = employee.GetStatistics();
+
+
 Console.WriteLine($"Average : {statistics.Average:N2}");
-Console.WriteLine($"Min : {statistics.Min}");
-Console.WriteLine($"Max : {statistics.Max}");
+Console.WriteLine($"Min     : {statistics.Min}");
+Console.WriteLine($"Max     : {statistics.Max}");
